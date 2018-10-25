02fdb646008be051c533275619b427ed53e38291
jerem <wiedman0@hm.edu>
2018-10-25 11:46:00 +0200

    Add project files.

---
+++ TravisCI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravisCI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int multiplication (int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravisCI;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Program program = new Program();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(program.multiplication(1, 2), 2);
+        }
+    }
+}
