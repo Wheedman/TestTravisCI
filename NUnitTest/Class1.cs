689ab76510f128280bebc1499ebf1c09da511fbf
jerem <wiedman0@hm.edu>
2018-10-25 12:58:13 +0200

    First NUnit

---
+++ NUnitTest/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TravisCI;
+
+namespace NUnitTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        private Program program = new Program();
+        [Test]
+        public void CalculationTest1()
+        {
+            Assert.AreEqual(program.multiplication(1, 2), 2);
+        }
+    }
+}
