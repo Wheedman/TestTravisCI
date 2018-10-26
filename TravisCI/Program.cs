e6f6223c2873ad155ec3ba42aefb0d4185e9e777
Kendy Nguyen <kendynguyen20@hotmail.com>
2018-10-26 10:30:25 +0300

    fixed merge

--- TravisCI/Program.cs
+++ TravisCI/Program.cs
@@ -14,11 +14,8 @@ namespace TravisCI
 
         public int multiplication (int a, int b)
         {
-<<<<<<< HEAD
 			// test
-=======
             //lets do this
->>>>>>> 7bd3b104c6b008e8e79e9bc1bb83211202f0df7b
             return a * b;
         }
     }
