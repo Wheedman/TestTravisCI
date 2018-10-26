3dab58a868d9ad18ef4e1df450e537ada187bc8b
Mikael M <mikael_96@live.com>
2018-10-26 10:06:41 +0300

    add comment

---
+++ NUnitTest/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TravisCI;
+
+namespace NUnitTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        private Program program = new Program();
+        [Test]
+        public void CalculationTest1()
+        {
+            Assert.AreEqual(program.multiplication(1, 2), 2);
+        }
+
+        [Test]
+        public void CalculationTest2()
+        {
+            Assert.AreEqual(program.multiplication(2, 2), 4);
+        }
+    }
+}
---
+++ TravisCI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravisCI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int multiplication (int a, int b)
+        {
+            //lets do this
+            return a * b;
+        }
+    }
+}
